ee7888a05383c4ad0e2c3676f7f75c7a75f5cbaa
An ByeongUk <quddnr005@naver.com>
2023-02-26 17:00:50 +0900

    javascript code to typescript

---
+++ 1-Type/1-2-function.ts
@@ -0,0 +1,28 @@
+{
+    // Javascript💩
+    function jsAdd (num1,num2)  {
+        return num1 + num2;
+}
+    // Typescript✨
+    function add (num1:number,num2:number):number  {
+        return num1 + num2;
+    }
+
+    // Javascript
+    function jsFetchNum(id) {
+        // code ...
+        // code ....
+        return new Promise((resolve, reject) => {
+            resolve(100);
+        })
+    }
+
+    // Typescript✨
+    function tsFetchNum(id:string):Promise<number> {
+        // code ...
+        // code ...
+        return new Promise((resolve,reject) => {
+            resolve(100);
+        })
+    }
+}
